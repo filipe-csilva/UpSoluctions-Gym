97347e379acab2b234461728210f05d3ca2ffb05
Filipe Silva <81836461+filipe-csilva@users.noreply.github.com>
2026-09-06 02:58:32 -0300

    Atualização do login

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.login');
+})->name('home');
+
+// Route::get('/', function () {
+//     return redirect()->route('login');
+// })->name('home');
 
 
 // Route::get('/', [AuthenticatedSessionController::class, 'create'])
